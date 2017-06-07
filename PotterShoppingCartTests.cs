43c86eb50b4b4e71a905a31048a7e807c716e7a0
chiachunlai <chiachun.lai@gmail.com>
2017-06-07 20:50:58 +0800

    測試案例 3~7 綠燈

--- PotterShoppingCartTests.cs
+++ PotterShoppingCartTests.cs
@@ -10,11 +10,7 @@ namespace PotterShoppingCart.Tests
     [TestClass()]
     public class PotterShoppingCartTests
     {
-        //[TestMethod()]
-        //public void AddToCartTest()
-        //{
-        //    Assert.Fail();
-        //}
+     
 
         [TestMethod()]
         public void 第一集買了一本_價格應為100元()
@@ -25,7 +21,7 @@ namespace PotterShoppingCart.Tests
             {
                 ISBN = "9789573317241",
                 Name = "哈利波特1",
-                Price = 100               
+                Price = 100
             };
 
             //act
@@ -42,12 +38,6 @@ namespace PotterShoppingCart.Tests
         {
             //arrange
             var target = new PotterShoppingCart();
-            var book = new Book
-            {
-                ISBN = "9789573317241",
-                Name = "哈利波特1",
-                Price = 100
-            };
 
             //act
             target.AddToCart(
@@ -73,5 +63,258 @@ namespace PotterShoppingCart.Tests
             var expected = 190;
             Assert.AreEqual(expected, totalPrice);
         }
+
+
+        [TestMethod()]
+        public void 一二三集各買了一本_價格應為270元()
+        {
+            //arrange
+            var target = new PotterShoppingCart();
+
+            //act
+            target.AddToCart(
+                new Book
+                {
+                    ISBN = "9573317249",
+                    Name = "哈利波特1",
+                    Price = 100
+                }
+            );
+            target.AddToCart(
+                new Book
+                {
+                    ISBN = "9573317583",
+                    Name = "哈利波特2",
+                    Price = 100
+                }
+            );
+            target.AddToCart(
+                new Book
+                {
+                    ISBN = "9573318008",
+                    Name = "哈利波特3",
+                    Price = 100
+                }
+            );
+
+            var totalPrice = target.CheckOut();
+
+            //assert
+            var expected = 270;
+            Assert.AreEqual(expected, totalPrice);
+        }
+
+        [TestMethod()]
+        public void 一二三四集各買了一本_價格應為320元()
+        {
+            //arrange
+            var target = new PotterShoppingCart();
+
+            //act
+            target.AddToCart(
+                new Book
+                {
+                    ISBN = "9573317249",
+                    Name = "哈利波特1",
+                    Price = 100
+                }
+            );
+            target.AddToCart(
+                new Book
+                {
+                    ISBN = "9573317583",
+                    Name = "哈利波特2",
+                    Price = 100
+                }
+            );
+            target.AddToCart(
+                new Book
+                {
+                    ISBN = "9573318008",
+                    Name = "哈利波特3",
+                    Price = 100
+                }
+            );
+
+            target.AddToCart(
+                new Book
+                {
+                    ISBN = "9573318318",
+                    Name = "哈利波特4",
+                    Price = 100
+                }
+            );
+
+            var totalPrice = target.CheckOut();
+
+            //assert
+            var expected = 320;
+            Assert.AreEqual(expected, totalPrice);
+        }
+
+        [TestMethod()]
+        public void 一次買了整套_一二三四五集各買了一本_價格應為375元()
+        {
+            //arrange
+            var target = new PotterShoppingCart();
+
+            //act
+            target.AddToCart(
+                new Book
+                {
+                    ISBN = "9573317249",
+                    Name = "哈利波特1",
+                    Price = 100
+                }
+            );
+            target.AddToCart(
+                new Book
+                {
+                    ISBN = "9573317583",
+                    Name = "哈利波特2",
+                    Price = 100
+                }
+            );
+            target.AddToCart(
+                new Book
+                {
+                    ISBN = "9573318008",
+                    Name = "哈利波特3",
+                    Price = 100
+                }
+            );
+
+            target.AddToCart(
+                new Book
+                {
+                    ISBN = "9573318318",
+                    Name = "哈利波特4",
+                    Price = 100
+                }
+            );
+
+            target.AddToCart(
+                new Book
+                {
+                    ISBN = "9573319861",
+                    Name = "哈利波特5",
+                    Price = 100
+                }
+            );
+
+            var totalPrice = target.CheckOut();
+
+            //assert
+            var expected = 375;
+            Assert.AreEqual(expected, totalPrice);
+        }
+
+        [TestMethod()]
+        public void 一二集各買了一本_第三集買了兩本_價格應為370元()
+        {
+            //arrange
+            var target = new PotterShoppingCart();
+
+            //act
+            target.AddToCart(
+                new Book
+                {
+                    ISBN = "9573317249",
+                    Name = "哈利波特1",
+                    Price = 100
+                }
+            );
+            target.AddToCart(
+                new Book
+                {
+                    ISBN = "9573317583",
+                    Name = "哈利波特2",
+                    Price = 100
+                }
+            );
+            target.AddToCart(
+                new Book
+                {
+                    ISBN = "9573318008",
+                    Name = "哈利波特3",
+                    Price = 100
+                }
+            );
+
+            target.AddToCart(
+                new Book
+                {
+                    ISBN = "9573318008",
+                    Name = "哈利波特3",
+                    Price = 100
+                }
+            );
+                      
+
+            var totalPrice = target.CheckOut();
+
+            //assert
+            var expected = 370;
+            Assert.AreEqual(expected, totalPrice);
+        }
+
+        [TestMethod()]
+        public void 第一集買了一本_第二三集各買了兩本_價格應為460元()
+        {
+            //arrange
+            var target = new PotterShoppingCart();
+
+            //act
+            target.AddToCart(
+                new Book
+                {
+                    ISBN = "9573317249",
+                    Name = "哈利波特1",
+                    Price = 100
+                }
+            );
+            target.AddToCart(
+                new Book
+                {
+                    ISBN = "9573317583",
+                    Name = "哈利波特2",
+                    Price = 100
+                }
+            );
+            target.AddToCart(
+                new Book
+                {
+                    ISBN = "9573317583",
+                    Name = "哈利波特2",
+                    Price = 100
+                }
+            );
+            target.AddToCart(
+                new Book
+                {
+                    ISBN = "9573318008",
+                    Name = "哈利波特3",
+                    Price = 100
+                }
+            );
+
+            target.AddToCart(
+                new Book
+                {
+                    ISBN = "9573318008",
+                    Name = "哈利波特3",
+                    Price = 100
+                }
+            );
+
+
+            var totalPrice = target.CheckOut();
+
+            //assert
+            var expected = 460;
+            Assert.AreEqual(expected, totalPrice);
+        }
+
+
     }
 }
